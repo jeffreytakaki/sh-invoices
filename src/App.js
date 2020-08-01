331858998a1347a35d327735e9784ded447486dc
@jtakaki <jtakaki@murad.com>
2020-07-31 21:36:54 -0700

    set up project, add views, hook up state to invoice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import { Header } from './components/Header'
+import  { Invoices } from './components/Invoices'
+import  { Invoice } from './components/Invoice'
 
 function App() {
-  return (
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
+        <Router>
+            <Switch>
+            <Route path="/list-view">
+                <h1>router list</h1>
+                <Invoices />
+            </Route>    
+            <Route path="/create">
+                <Invoice />
+            </Route>
+            <Route path="/edit/:id">
+                <Invoice />
+            </Route>
+            <Route path="/">
+
+            </Route>
+            </Switch>
+        </Router>
     </div>
-  );
+    );
 }
 
 export default App;
